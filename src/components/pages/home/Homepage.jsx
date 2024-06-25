e3cba2d10e7d8fd8f6d74c0c54127d86b96ef88f
Beverly Ifeobu <beverlyifeobu@gmail.com>
2024-06-25 14:53:07 -0400

    importing img1

--- src/components/pages/home/Homepage.jsx
+++ src/components/pages/home/Homepage.jsx
@@ -2,6 +2,7 @@ import Title from '../../Title'
 import './Homepage.css'
 // import gif from 'src/assets/peace-background.gif'
 import gif from '../../../assets/peace-background.gif'
+import img1 from '../../../assets/young-girl-online-studying-at-home.jpg'
 
 function Homepage() {
 
@@ -13,6 +14,9 @@ function Homepage() {
         <>
             <img src={gif} className='background' alt=''/>
             <Title>Welcome to<br/><p className="studySol">Study Solitude</p></Title>
+            <div className='imgBox'>
+
+            </div>
         </>
     )
 }
