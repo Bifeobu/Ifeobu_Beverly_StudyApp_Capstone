8cfb45cc19c094c0fabfe3acc2c8243a7a8f77c6
Beverly Ifeobu <beverlyifeobu@gmail.com>
2024-06-25 11:13:35 -0400

    importing homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Route, Routes } from "react-router-dom";
 import './App.css';
+import Homepage from './components/pages/home/Homepage';
 
 function App() {
 
--- src/components/pages/home/Homepage.jsx
+++ src/components/pages/home/Homepage.jsx
@@ -1,4 +1,4 @@
-import Title from './components/Title';
+import Title from '../../Title'
 import './Homepage.css'
 
 function Homepage() {
