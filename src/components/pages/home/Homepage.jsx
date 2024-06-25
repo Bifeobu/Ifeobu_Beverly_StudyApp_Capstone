786b4e22ec89912404b548d0fa0064f545c2fc0e
Beverly Ifeobu <beverlyifeobu@gmail.com>
2024-06-25 19:42:00 -0400

    imported navbar

--- src/components/pages/home/Homepage.jsx
+++ src/components/pages/home/Homepage.jsx
@@ -1,6 +1,7 @@
 import Title from '../../Title'
 import './Homepage.css'
 import gif from '../../../assets/peace-background.gif'
+import Navbar from '../../navbar/Navbar'
 // import img1 from '../../../assets/young-girl-online-studying-at-home.jpg'
 // import img2 from '../../../assets/person-studying-college-student-clipart.png'
 // import img3 from '../../../assets/homework-study-skills-writing-student-clipart.jpg'
@@ -14,6 +15,7 @@ function Homepage() {
     return (
         <>
             <img src={gif} className='background' alt='' />
+            <Navbar/>
             <Title>Welcome to<br /><p className="studySol">Study Solitude</p></Title>
             {/* <div className='imgBox1'>
                 <img src={img1} className='img1' />
