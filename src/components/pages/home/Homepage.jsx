74f783e82c89a43218cddbe393e87135816400d7
Beverly Ifeobu <beverlyifeobu@gmail.com>
2024-06-25 18:27:23 -0400

    rid the image all together

--- src/components/pages/home/Homepage.jsx
+++ src/components/pages/home/Homepage.jsx
@@ -1,8 +1,7 @@
 import Title from '../../Title'
 import './Homepage.css'
-// import gif from 'src/assets/peace-background.gif'
 import gif from '../../../assets/peace-background.gif'
-import img1 from '../../../assets/young-girl-online-studying-at-home.jpg'
+// import img1 from '../../../assets/young-girl-online-studying-at-home.jpg'
 // import img2 from '../../../assets/person-studying-college-student-clipart.png'
 // import img3 from '../../../assets/homework-study-skills-writing-student-clipart.jpg'
 
@@ -16,9 +15,9 @@ function Homepage() {
         <>
             <img src={gif} className='background' alt='' />
             <Title>Welcome to<br /><p className="studySol">Study Solitude</p></Title>
-            <div className='imgBox1'>
+            {/* <div className='imgBox1'>
                 <img src={img1} className='img1' />
-            </div>
+            </div> */}
         </>
     )
 }
