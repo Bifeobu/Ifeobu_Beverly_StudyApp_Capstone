c771316ca58a451ec77af039c6e278fba92414f5
Beverly Ifeobu <beverlyifeobu@gmail.com>
2024-06-25 15:45:32 -0400

    changing img again

--- src/components/pages/home/Homepage.jsx
+++ src/components/pages/home/Homepage.jsx
@@ -2,8 +2,9 @@ import Title from '../../Title'
 import './Homepage.css'
 // import gif from 'src/assets/peace-background.gif'
 import gif from '../../../assets/peace-background.gif'
-// import img1 from '../../../assets/young-girl-online-studying-at-home.jpg'
-import img2 from '../../../assets/person-studying-college-student-clipart.png'
+import img1 from '../../../assets/young-girl-online-studying-at-home.jpg'
+// import img2 from '../../../assets/person-studying-college-student-clipart.png'
+// import img3 from '../../../assets/homework-study-skills-writing-student-clipart.jpg'
 
 function Homepage() {
 
@@ -17,7 +18,7 @@ function Homepage() {
             <Title>Welcome to<br/><p className="studySol">Study Solitude</p></Title>
             <div className='container1'>
                 <div className='imgBox1'>
-                    <img src={img2} className='img1'/>
+                    <img src={img1} className='img1'/>
                 </div>
             </div>
         </>
