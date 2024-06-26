a074e3493033c7341cb9829a7c2e4af00204a9c7
Beverly Ifeobu <beverlyifeobu@gmail.com>
2024-06-26 06:38:56 -0400

    about us lorem

--- src/components/pages/about/AboutUs.jsx
+++ src/components/pages/about/AboutUs.jsx
@@ -1,9 +1,12 @@
+import Navbar from "../../navbar/Navbar";
+
 function AboutUs() {
 
     return(
         <>
         <Navbar/>
         <h1 className="about">About Us</h1>
+        <h3 className="lorem">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis porro ea debitis quae ducimus aliquid perspiciatis accusamus, nobis autem facere vitae, blanditiis accusantium quos, asperiores adipisci nam. Veniam officiis a pariatur, cum et mollitia ad accusamus ratione recusandae enim doloribus totam beatae ex. Earum dolore, beatae laboriosam quo at eum omnis mollitia ex dolores tenetur. Maxime nisi quas est quos explicabo dolorem libero voluptate repellendus cum? Rerum illum nesciunt rem dolor! Et libero fuga non ea. At minus eius, sint debitis quae nostrum nesciunt magni nihil totam voluptate architecto, enim nemo fuga error velit quidem temporibus odio qui? Incidunt, ratione!</h3>
         </>
     )
 }
