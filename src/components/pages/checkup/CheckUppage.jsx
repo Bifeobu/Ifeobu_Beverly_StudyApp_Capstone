37b248c6b57b6de5e17eec6f31f70a03665cf21c
Beverly Ifeobu <beverlyifeobu@gmail.com>
2024-06-26 03:01:28 -0400

    css made

--- src/components/pages/checkup/CheckUppage.jsx
+++ src/components/pages/checkup/CheckUppage.jsx
@@ -1,4 +1,5 @@
 import { useRef } from 'react';
+import './CheckUppage.css'
 
 function CheckUppage({ checkForm, setCheckForm, BASE_URL }) {
     const moodRef = useRef(null)
@@ -9,52 +10,54 @@ function CheckUppage({ checkForm, setCheckForm, BASE_URL }) {
         //prevent webpage from refreshing after submitting
         e.preventDefault()
         try {
-          const newCheckForm = {
-            mood: moodRef.current.value,
-            content: conRef.current.value,
-            username: nameRef.current.value
-          }  
+            const newCheckForm = {
+                mood: moodRef.current.value,
+                content: conRef.current.value,
+                username: nameRef.current.value
+            }
 
-          //options object("{}") to specifiy what kind of request to be made
-          const response = await fetch(`${BASE_URL}/check-in`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            //new form goes into body but must be turned into a string
-            body: JSON.stringify(newCheckForm)
-          });
+            //options object("{}") to specifiy what kind of request to be made
+            const response = await fetch(`${BASE_URL}/check-in`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                //new form goes into body but must be turned into a string
+                body: JSON.stringify(newCheckForm)
+            });
 
-          //if status of response is not successful(201), return or do nothing 
-          if (response.status !== 201) {
-            return
-          }
+            //if status of response is not successful(201), return or do nothing 
+            if (response.status !== 201) {
+                return
+            }
 
-          //turn response to json
-          const createdForm = await response.json();
+            //turn response to json
+            const createdForm = await response.json();
 
-          //pass in all of the forms/data that was already there
-          setCheckForm([...checkForm, createdForm]);
+            //pass in all of the forms/data that was already there
+            setCheckForm([...checkForm, createdForm]);
 
-          //after checkForm state is set, this will reset the typing box back to nothing so user can add a new entry
-          moodRef.current.value = "";
-          conRef.current.value = "";
-          nameRef.current.value = "";
+            //after checkForm state is set, this will reset the typing box back to nothing so user can add a new entry
+            moodRef.current.value = "";
+            conRef.current.value = "";
+            nameRef.current.value = "";
 
         } catch (err) {
             console.log(err)
         }
     }
 
-    return(
-        <form onSubmit={handleSubmit} className='checkForm'>
-            Mood: <input type="text" ref={moodRef}placeholder='Ecstatic, Happy, Neutral, Sad, or Angry?'/>
-            <br/>
-            Content: <input type="text" ref={conRef} placeholder='Explain your feelings...'/>
-            <br/>
-            Username: <input type="text" ref={nameRef}placeholder='Username'/>
-            <br/>
-        </form>
+    return (
+        <div className="formContainer">
+            <form onSubmit={handleSubmit} className='checkForm'>
+                Mood: <input type="text" ref={moodRef} placeholder='Ecstatic, Happy, Neutral, Sad, or Angry?' />
+                <br />
+                Content: <input type="text" ref={conRef} placeholder='Explain your feelings...' />
+                <br />
+                Username: <input type="text" ref={nameRef} placeholder='Username' />
+                <br />
+            </form>
+        </div>
     )
 }
 
